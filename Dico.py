e76ba28ccc18704f0e2ddbda97efea6159dc57a2
CorentinPresvots <144250214+CorentinPresvots@users.noreply.github.com>
2025-01-27 17:45:53 +0100

    Dico.py

--- Dico.py
+++ Dico.py
@@ -35,31 +35,35 @@ class Dico_Cos_Sin_Dirac():
         nb_vect_freq = int(self.f_factor * f_max * 2 * N / self.fs)
         
         # Initialize the impulse dictionary
-        I = np.zeros((N, N))
         
-        # Initialize a constant basis vector
-        CST = np.ones((N, 1)) / np.sqrt(N)
-        
-        # Initialize the matrix to hold cosine and sine basis vectors
-        F = np.zeros((N, 2 * nb_vect_freq))
-        
+       
         # Construct the impulse dictionary
+  
+        I = np.zeros((N, N))  # Initialize the impulse dictionary
         for raw in range(0, N):
             for col in range(0, int(N / 2)):
                 if raw == col:
                     I[raw, 2 * col] = 1
                     I[N - raw - 1, 2 * col + 1] = 1
+    
+        # or
+        #I = np.eye(N)
+        
+        # Initialize the matrix to hold cosine and sine basis vectors
+        F = np.zeros((N, 2 * nb_vect_freq))
+                
 
         # Construct the cosine and sine dictionary
         for raw in range(0, N):
             for col in range(0, nb_vect_freq):
                 # Cosine basis vectors
-                F[raw, 2 * col] = np.sqrt(2 / N) * np.cos((np.pi / N) * (raw + 1 / 2) * ((col + 1)) / self.f_factor)
+                F[raw, 2 * col] = np.sqrt(2 / N) * np.cos((np.pi / N) * (raw + 1 / 2) * (col) / self.f_factor)
                 # Sine basis vectors
-                F[raw, 2 * col + 1] = np.sqrt(2 / N) * np.sin((np.pi / N) * (raw + 1 / 2) * ((col + 1) / self.f_factor))
+                F[raw, 2 * col+1] = np.sqrt(2 / N) * np.sin((np.pi / N) * (raw + 1 / 2) * ((col + 1) / self.f_factor))
+               
                 
         # Concatenate all basis vectors into a single dictionary
-        D = np.concatenate((CST, F, I), axis=1)
+        D = np.concatenate((F, I), axis=1)
         return D
 
 if __name__ == "__main__":
@@ -68,7 +72,7 @@ if __name__ == "__main__":
     
     f_factor = 1  # Frequency scaling factor for the dictionary
     fs = 6400       # Sampling frequency (Hz)
-    f_max = 300     # Maximum frequency in the dictionary (Hz)
+    f_max = fs / 2     # Maximum frequency in the dictionary (Hz)
     
     # Initialize the dictionary generator
     Dico = Dico_Cos_Sin_Dirac(fs, f_factor)
@@ -108,7 +112,7 @@ if __name__ == "__main__":
     plt.grid(which='minor', color='#999999', linestyle='-', alpha=0.2)
 
     # Plot the cosine and sine vectors
-    for j in range(1, int((size_D - N) / 2) + 1):
+    for j in range(1, int((size_D - N) / 2) + 1,int(N/4)):
         fig = plt.figure(figsize=(8, 4), dpi=70)
         plt.plot(t, D[:, 2 * j - 1], lw=2, label="Cosine {} Hz".format(int(j * fs / (2 * N * f_factor))))
         plt.plot(t, D[:, 2 * j], lw=2, label="Sine {} Hz".format(int(j * fs / (2 * N * f_factor))))
